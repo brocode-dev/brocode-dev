313b9c3062aa076acdea8a36d06581e96e68e4a2
Rupesh Vyas <rupeshvyas@thoughtwin.com>
2023-05-17 14:16:37 +0530

    fix: dotenv

--- brocode/brocode/settings/base.py
+++ brocode/brocode/settings/base.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'brocode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
--- brocode/brocode/wsgi.py
+++ brocode/brocode/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
-doteenv_path = os.path.join(os.path.dirname(__file__),'.env')
+
+doteenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(doteenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brocode.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
